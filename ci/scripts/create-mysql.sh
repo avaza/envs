48bde8247f7de2ba23affe3fed3cad1901300c92
avazagit <j.murray@avaza.co>
2015-02-10 10:29:22 -0600

    Revert "multi"
    This reverts commit 51114bb971576bb99a55a035e16c41eb160899cd.

--- ci/scripts/create-mysql.sh
+++ ci/scripts/create-mysql.sh
@@ -3,4 +3,4 @@
 DB=$1;
 mysql -uhomestead -psecret -e "DROP DATABASE IF EXISTS $DB";
 mysql -uhomestead -psecret -e "CREATE DATABASE $DB";
-mysql -uhomestead -psecret -D$1 -e "source /code/$DB/$DB_db.sql";
+mysql -uhomestead -psecret -D$1 -e "source /code/db_design.sql";
