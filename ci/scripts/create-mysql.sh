5f4f1e8f64ced879b0a1831417929f15e4516f31
avazagit <j.murray@avaza.co>
2015-02-18 13:30:54 -0600

    SQL filename as app-name

--- ci/scripts/create-mysql.sh
+++ ci/scripts/create-mysql.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 DB=$1;
+NM="_db.sql"
 mysql -uhomestead -psecret -e "DROP DATABASE IF EXISTS $DB";
 mysql -uhomestead -psecret -e "CREATE DATABASE $DB";
-mysql -uhomestead -psecret -D$1 -e "source /code/$DB/$DB_db.sql";
+mysql -uhomestead -psecret -D$1 -e "source /code/$DB/$DB$NM";
