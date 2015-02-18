115e0d2f1c1638d4994b101cb3fda9df10a130d2
avazagit <j.murray@avaza.co>
2015-02-18 13:34:07 -0600

    Complete Multi

--- ci/scripts/create-mysql.sh
+++ ci/scripts/create-mysql.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 DB=$1;
-NM="_db.sql"
+EXT=".sql"
 mysql -uhomestead -psecret -e "DROP DATABASE IF EXISTS $DB";
 mysql -uhomestead -psecret -e "CREATE DATABASE $DB";
-mysql -uhomestead -psecret -D$1 -e "source /code/$DB/$DB$NM";
+mysql -uhomestead -psecret -D$1 -e "source /code/$DB/$DB$EXT";
